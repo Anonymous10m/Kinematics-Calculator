b3686e1854ec899c5654ac147f646eab1e25cc24
LordCod3r <64400282+Anonymous10m@users.noreply.github.com>
2023-10-02 14:19:37 +0530

    main.py

--- main.py
+++ main.py
@@ -447,22 +447,28 @@ elif required == "t":
     
     ax1.plot(t_range,acc, label = "Acceleration")
     ax2.plot(t_range,v, 'orange', label = "Velocity")
-    ax3.plot(t_range,s, 'green', label = "Distance")    
+    ax3.plot(t_range,s, 'green', label = "Distance")
+    
     ax1.set_title('Acceleration(m\s^2) v/s Time(s)')
     ax1.set_xlabel('Time(s)')
-    ax1.set_ylabel('Acceleration(m/s^2)')   
+    ax1.set_ylabel('Acceleration(m/s^2)')
+   
     ax2.set_title('Velocity(m/s) v/s Time(s)')
     ax2.set_xlabel('Time(s)')
     ax2.set_ylabel('Velocity(m/s)') 
+
     ax3.set_title('Distance(m) v/s Time(s)')
     ax3.set_xlabel('Time(s)')
-    ax3.set_ylabel('Distance(m)')   
+    ax3.set_ylabel('Distance(m)') 
+  
     ax1.grid()
     ax2.grid()
     ax3.grid()  
+
     ax1.legend(loc = 'upper left')
     ax2.legend(loc = 'upper left')
     ax3.legend(loc = 'upper left')  
+
     plt.subplots_adjust(hspace=0.6)
     fig.suptitle("Graphs of the Given Motion")
     plt.show()  
